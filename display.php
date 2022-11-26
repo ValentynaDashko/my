9375b71fa244659e101f6ab28d65be7004123239
AlonaDashko <dashkoalena@gmail.com>
2022-11-26 20:50:04 +0100

    first commit

---
+++ display.php
@@ -0,0 +1,7 @@
+<?php
+$name = $_POST["firstname"];
+$surname = $_POST["lastname"];
+$age = $_POST["age"];
+echo "Ваше имя: <b>".$name . " " . $surname . "<br></b>";
+echo "Ваш возраст: <b>".$age . " лет" . "</b>";
+?>
